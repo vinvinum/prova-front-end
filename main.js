b38830185d052670d9b56bfd9c754669beb0d4d4
vinvinum <vin.vinum0@gmail.com>
2025-04-25 20:57:35 -0300

    codigo para adicionar, excluir e marcar tarefas adicionado

---
+++ main.js
@@ -0,0 +1,28 @@
+var dadosLista = [];
+
+function salvarTarefa(){
+    let tarefa= document.getElementById('tarefa').value;
+
+    if (tarefa){
+        dadosLista.push(tarefa);
+        document.getElementById('tarefa').value = "";
+        criarLista();
+    } else{
+        alert("Campo de tarefas não preenchido");
+    }
+
+}
+
+function criarLista(){
+    let tabela =  "<tr><th>Tarefas</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i++){
+        tabela += "<tr><td>" +dadosLista[i] + "</td><td><input type='checkbox' id='checkbox'</button><button class='btn btn-danger' id='btnexcluir' onclick='excluir(this.parentNode.parentNode.rowIndex)'>X</button> </td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
+}
